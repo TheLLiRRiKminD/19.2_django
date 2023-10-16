ba9ec2a2f079451d786cf9f556b15e06e80ae9ce
LLiRRiK <kill2002@mail.ru>
2023-10-16 19:07:21 +0300

    add url controllers and templates stable version 1.0

--- catalog/views.py
+++ catalog/views.py
@@ -3,5 +3,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'catalog/index.html')
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        name = request.POST.get("name")
+        message = request.POST.get("message")
+        print(f"{name} ({email}): {message}")
+    return render(request, 'catalog/contacts.html')
