294ab28a8b1ae1de2dfc0d9aeb06242a662b068d
TheLLiRRiKminD <kill2002@mail.ru>
2023-11-19 17:28:28 +0300

    Make migrations

--- catalog/models.py
+++ catalog/models.py
@@ -5,14 +5,14 @@ NULLABLE = {'blank': True, 'null': True}
 
 class Category(models.Model):
     name = models.CharField(max_length=50, verbose_name='Наименование')
-    description = models.TextField(verbose_name='описание')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
 
 
 class Product(models.Model):
     name = models.CharField(max_length=50, verbose_name='Наименование')
-    description = models.TextField(verbose_name='описание')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
     image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     purchase_price = models.FloatField(verbose_name='цена за покупку')
-    date_of_creation = models.DateField(verbose_name='дата создания')
-    last_modified_date = models.DateField(verbose_name='дата последнего изменения')
+    date_of_creation = models.DateField(verbose_name='дата создания', auto_now_add=True)
+    last_modified_date = models.DateField(verbose_name='дата последнего изменения', auto_now=True)
