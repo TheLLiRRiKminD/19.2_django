87aa7a81542158a2b1c4a88352821cdd3f897091
TheLLiRRiKminD <kill2002@mail.ru>
2023-12-03 13:05:02 +0300

    Full DB filling, revision 1

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -10,24 +10,25 @@ class Command(BaseCommand):
         products_list = [
             {"name": "iPhone 13",
              "description": "Новый смартфон от Apple с передовыми технологиями и камерой высокого разрешения.",
-             "purchase_price": 999.99, "category": Category.objects.get(id=3)},
+             "purchase_price": 999.99, "category": Category.objects.get_or_create(name='Смартфоны')[0]},
             {"name": "Dell XPS 13",
              "description": "Легкий и мощный ноутбук с высоким разрешением дисплея и производительным процессором.",
-             "purchase_price": 1299.99, "category": Category.objects.get(id=1)},
+             "purchase_price": 1299.99, "category": Category.objects.get_or_create(name='Ноутбуки и ПК')[0]},
             {"name": "Sony Bravia 4K TV",
              "description": "4K телевизор с HDR и технологией Acoustic Surface для отличного звука.",
-             "purchase_price": 1499.99, "category": Category.objects.get(id=2)},
+             "purchase_price": 1499.99, "category": Category.objects.get_or_create(name='Телевизоры и Аудио')[0]},
             {"name": "Canon EOS R5",
              "description": "Зеркальная камера с высоким разрешением и возможностью съемки 8K видео.",
-             "purchase_price": 2499.99, "category": Category.objects.get(id=7)},
+             "purchase_price": 2499.99, "category": Category.objects.get_or_create(name='Фото- и Видеокамеры')[0]},
             {"name": "Apple Watch Series 7",
              "description": "Умные часы с крупным дисплеем и множеством функций для отслеживания здоровья.",
-             "purchase_price": 399.99, "category": Category.objects.get(id=8)},
+             "purchase_price": 399.99, "category": Category.objects.get_or_create(name='Гаджеты и Умные устройства')[0]},
             {"name": "PlayStation 5",
              "description": "Последняя игровая консоль от Sony с высокой производительностью и поддержкой 4K графики.",
-             "purchase_price": 499.99, "category": Category.objects.get(id=9)},
+             "purchase_price": 499.99, "category": Category.objects.get_or_create(name='Игровые консоли и Аксессуары')[0]},
             {"name": "AirPods Pro", "description": "Беспроводные наушники с активным шумоподавлением от Apple.",
-             "purchase_price": 249.99, "category": Category.objects.get(id=10)}
+             "purchase_price": 249.99,
+             "category": Category.objects.get_or_create(name='Аксессуары и Зарядные устройства')[0]}
         ]
 
         products_for_create = []
