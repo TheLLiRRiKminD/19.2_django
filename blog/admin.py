598ced96f420c980d2e7560f3bc8097388b7e380
TheLLiRRiKminD <kill2002@mail.ru>
2023-12-24 14:50:20 +0300

    Forms, rev.4

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from blog.models import BlogWriter
+
+
 # Register your models here.
+@admin.register(BlogWriter)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'image', 'is_published')
+    list_filter = ('title',)
+    search_fields = ('title', 'content')
